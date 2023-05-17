d894412fa97b81899a805193a8d6449a6c3cd5ec
samandar12 <samandarkomiljonov1212@gmail.com>
2023-05-17 21:50:55 +0500

    Bu js5-dars kodlari yuklandi

---
+++ JS/main.js
@@ -0,0 +1,40 @@
+/*let headTag = document.head;
+let bodyTag = document.body;
+let havola = document.documentURI;
+let formalar = document.forms;
+let rasmlar = document.images;
+let havolalar = document.links;
+
+console.log(havolalar)*/
+
+/*function meniBOS(){
+    alert("Siz tugmani bostingiz!");
+}
+function meniIkkiMartaBos(){
+    alert("Siz tugmani 2marta bostingiz!");
+}
+function tanlash(event){
+    alert( "Siz "    +    event.target.value   +   " mashinasini tanladingiz!");
+}
+function inputgaBosish(){
+    console.log("Inputga Bosildi!");
+}
+function inputdanChiqish(){
+    console.log("huatdsiyutfgew8agy")
+}
+function yozish(event){
+    console.log(event.target.value)
+}
+
+document.querySelector(".text").addEventListener('copy',function (event){
+    console.log("Diqqat Sizdan "+event.target.value+" nomli fayl ko'chirilmoqda!")
+})
+document.querySelector(".text").addEventListener('paste',function (event){
+    console.log("Diqqat Sizdan "+event.target.value+" nomli fayl ko'chirilmoqda!")
+})*/
+function harakatlanish(e){
+    console.log(e.clientY,e.clientX);
+}
+
+
+
