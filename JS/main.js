a8928d39ab29850ed418d2725f3106ea8bde790f
samandar12 <samandarkomiljonov1212@gmail.com>
2023-05-21 19:30:21 +0500

    js 5-dars
    bu js 5-dras kodlari

--- JS/main.js
+++ JS/main.js
@@ -1,40 +1,49 @@
-/*let headTag = document.head;
+/*
+JSda HTML teglari bilan ishlash:
+let headTag = document.head;
 let bodyTag = document.body;
 let havola = document.documentURI;
 let formalar = document.forms;
 let rasmlar = document.images;
 let havolalar = document.links;
+console.log(havolalar);
+*/
+/* Events in JS: onclick,ondblclick,onchange */
 
-console.log(havolalar)*/
-
-/*function meniBOS(){
-    alert("Siz tugmani bostingiz!");
+function meniBos(){
+    alert("Siz tugmani bosdingiz!");
 }
 function meniIkkiMartaBos(){
-    alert("Siz tugmani 2marta bostingiz!");
+    alert("Siz tugmani 2 marta bosdingiz!");
 }
 function tanlash(event){
-    alert( "Siz "    +    event.target.value   +   " mashinasini tanladingiz!");
+    alert("Siz " + event.target.value + " mashinasini tanladingiz!");
 }
 function inputgaBosish(){
-    console.log("Inputga Bosildi!");
+    console.log("Inputga bosildi!");
 }
 function inputdanChiqish(){
-    console.log("huatdsiyutfgew8agy")
+    console.log("Inputdan chiqildi!")
 }
 function yozish(event){
-    console.log(event.target.value)
+    console.log(event.target.value);
 }
-
-document.querySelector(".text").addEventListener('copy',function (event){
-    console.log("Diqqat Sizdan "+event.target.value+" nomli fayl ko'chirilmoqda!")
-})
-document.querySelector(".text").addEventListener('paste',function (event){
-    console.log("Diqqat Sizdan "+event.target.value+" nomli fayl ko'chirilmoqda!")
-})*/
+document.querySelector('.text').addEventListener('copy',function (event){
+    console.log("Diqqat! Sizdan "+ event.target.value + " nomli fayl ko'chrilyapti!");
+});
 function harakatlanish(e){
     console.log(e.clientY,e.clientX);
 }
+function kirish(){
+    alert("Hududga kirildi!");
+}
+function chiqish(){
+    alert("Hudud tark etildi!");
+}
+
+
+
+
 
 
 
